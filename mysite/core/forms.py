3d03b3c571782be977c1bc9fd27457d2c124e5b9
mustack <mnadaf@zscaler.com>
2022-02-22 12:59:47 +0530

    first commit

--- mysite/core/forms.py
+++ mysite/core/forms.py
@@ -1,9 +1,5 @@
 from django import forms
 
-from .models import Book
 
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ('title', 'author', 'pdf', 'cover')
+
