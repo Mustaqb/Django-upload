a391a762518b3d13e195f30801720b99106d5815
Mustaqb <mushtaqn1604@gmail.com>
2022-02-23 23:15:47 +0530

    file upload fixed

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView, CreateView
 from django.core.files.storage import FileSystemStorage
 from django.urls import reverse_lazy
-
+import os
 
 
 
@@ -11,9 +11,11 @@ def upload(request):
     if request.method == 'POST':
         try:
             uploaded_file = request.FILES['document']
-            fs = FileSystemStorage()
-            name = fs.save(uploaded_file.name, uploaded_file)
-            context['url'] = fs.url(name)
+            file_size=uploaded_file.size
+            # fs = FileSystemStorage()
+            # name = fs.save(uploaded_file.name, uploaded_file)
+            context['url'] = 'succesfully'
+            context['file_size'] = file_size
         except:
             pass
     return render(request, 'upload.html', context)
